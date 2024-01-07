9c4db08f2e8e10cf76378c1ddb2c17ed1c867b9d
Alix <ehongoa@yahoo.fr>
2024-01-07 13:38:11 -0500

    code

--- ec2.tf
+++ ec2.tf
@@ -6,10 +6,3 @@ resource "aws_instance" "server1" {
   }
 }
 
-resource "aws_instance" "server2" {
-  instance_type = var.instance_type
-  ami = data.aws_ami.ami1.id
-  tags = {
-    name = "terraform-server2"
-  }
-}
